9063c1249e962e5c9de229c7fad7dad9f6806b00
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2023-10-25 19:16:13 +0200

    concatenazione
    concatenazione

---
+++ main.js
@@ -0,0 +1,15 @@
+let nomeUtente = prompt("Inserisci il tuo nome");
+console.log(nomeUtente);
+
+
+
+let cognomeUtente = prompt("Inserisci il tuo cognome");
+console.log(cognomeUtente);
+
+let coloreUtente = prompt("Inserisci il tuo colore preferito");
+console.log(coloreUtente);
+
+let numero= "#23"
+
+
+document.getElementById("password").innerHTML = "La tua password è: " + nomeUtente + cognomeUtente +  coloreUtente + numero;
